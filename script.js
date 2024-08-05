f90ceef6ff862594bdd72f740eb06a9b8ec23136
Aisha <fatimaaisha002@gmail.com>
2024-08-05 15:58:44 +0500

    UP

---
+++ script.js
@@ -0,0 +1,50 @@
+//qNO1
+// let array=[1,89,70,45,33]
+// function reverse1(array) {
+    
+//     alert("yor array"+array.reverse() )
+   
+       
+
+
+// }
+
+// let largerArray=[1,23,90,54,88,60,11,33,77]
+// function now(){
+    
+//     return largerArray.reverse()
+// }
+// console.log(now(largerArray))
+
+//qno2
+// let userInput=+prompt("Enter the number of your choice")
+// if (userInput=== )) {
+    
+// }
+    
+// }
+// function num(array){
+//     return array=userInput
+    
+// }
+
+//QNO10
+// function myarray(arr,itemtofind) {
+  
+//     for (let i = 0; i < arr.length; i++) {
+//        if(arr[3]==itemtofind){
+//         return[3]
+
+//        }
+       
+//        else  if(arr[i]!==itemtofind){
+//         return-1
+//     }
+        
+//     }
+// }
+// const arr=[1,90,23,67,87,88,56]
+// const itemtofind=67
+// console.log(myarray(arr,itemtofind))
+
+//QNO6
